da9aa24cca85aecab3d206550e9dcb8fae3be532
Ellis Crawford <ellicraw@gmail.com>
2018-10-16 11:45:36 -0400

    file transfer

--- eclient.c
+++ eclient.c
@@ -1,40 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-
 #include <netinet/in.h>
-#include <unistd.h>
+#include <netdb.h> 
+#include<ctype.h>
 
-//client
-int main() {
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
 
-    // create a socket
-    int network_socket;
-    network_socket = socket(AF_INET, SOCK_STREAM, 0);
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
 
-    // specify an address for the socket
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(9002);
-    server_address.sin_addr.s_addr = INADDR_ANY;
-
-    int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
-    
-    //check for error with the connection
-    if (connection_status == -1) {
-        printf("There was an error making a connection to the socket \n\n");
+    char buffer[512];
+    if (argc < 3)
+    {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
     }
-
-    //receieve data from the server
-    char server_response[256];
-    recv(network_socket, &server_response, sizeof(server_response), 0);
-
-    //print out the server's response 
-    printf("The server sent the data: %s\n", server_response);
-
-    //close the socket
-    close(network_socket);
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+  
+        bzero(buffer,512);
+        
+    FILE *f;
+    
+    int words = 0;
+    char c;
+     f=fopen("sample.txt","r");
+    while((c=getc(f))!=EOF)			//Counting No of words in the file
+	{	
+		fscanf(f , "%s" , buffer);
+		if(isspace(c)||c=='\t')
+		words++;	
+	}
+	//printf("Words = %d \n"  , words);	//Ignore
+       
+      
+	write(sockfd, &words, sizeof(int));
+     	rewind(f);
+      
+            /*      fseek(f, 0L, SEEK_END);    	// tells size of the file. Not rquired for the functionality in code.
+	int sz = ftell(f);				//Just written for curiosity.
+	printf("Size is %d \n" , sz);
+          rewind(f);  
+            */
+       
+    char ch ;
+       while(ch != EOF)
+      {
+		
+		fscanf(f , "%s" , buffer);
+		//printf("%s\n" , buffer);	//Ignore
+		write(sockfd,buffer,512);
+		ch = fgetc(f);
+      }
+	printf("The file was sent successfully");
+    
+    close(sockfd);
     return 0;
 }
--- eserve.c
+++ eserve.c
@@ -1,39 +1,67 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
-
-#include <sys/types.h>
-#include <sys/socket.h>
-
-#include <netinet/in.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <ctype.h>
 
-//server
-int main() {
-    
-    char server_message[256] = "You have reached the server!";
 
-    // create a socket
-    int server_socket;
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
 
-    // specify an address for the socket
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(9002);
-    server_address.sin_addr.s_addr = INADDR_ANY;
-
-    // bind the socket to our specified IP and port
-    bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-
-    listen(server_socket, 5);
-
-    int client_socket;
-    client_socket = accept(server_socket, NULL, NULL);
-
-    // send the message
-    send(client_socket, server_message, sizeof(server_message), 0);
-
-    // close the socket
-    close(server_socket);
-    return 0;
+int main(int argc, char *argv[])
+{
+     int sockfd, newsockfd, portno;
+     socklen_t clilen;
+     char buffer[512];
+     struct sockaddr_in serv_addr, cli_addr;
+     int n;
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(sockfd, 
+                 (struct sockaddr *) &cli_addr, 
+                 &clilen);
+     if (newsockfd < 0) 
+          error("ERROR on accept");
+          
+          
+          
+          FILE *fp;
+         int ch = 0;
+            fp = fopen("sample_receive.txt","a");            
+            int words;
+		read(newsockfd, &words, sizeof(int));
+            //printf("Passed integer is : %d\n" , words);      //Ignore , Line for Testing
+          while(ch != words)
+       	   {
+        	 read(newsockfd , buffer , 512); 
+	   	 fprintf(fp , " %s" , buffer);   
+		 //printf(" %s %d "  , buffer , ch); //Line for Testing , Ignore
+		 ch++;
+	   }
+     	printf("The file was received successfully\n");
+	   printf("The new file created is sample_receive.txt");
+     close(newsockfd);
+     close(sockfd);
+     return 0; 
 }
